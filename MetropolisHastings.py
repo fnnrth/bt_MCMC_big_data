3743bbd898a04b278bce98cd904560fa3cd612a4
Fynn Orth <fynnorth@gmail.com>
2024-01-15 11:19:09 +0100

    test

--- MetropolisHastings.py
+++ MetropolisHastings.py
@@ -4,15 +4,16 @@ import concurrent.futures
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import numpy.random as npr
 import time
 
 class MetropolisHastings():
     def __init__(self, dataset):
-        self.dataset = dataset
+        #self.dataset = dataset
         self.N = dataset.size(0)
 
     def run(self, T, theta, data):
-        S = torch.zeros(T, theta.size(0))
+        S = np.zeros(T, theta.size(0))
         S[0,:] = theta
         for i in range(T-1):
             S[i+1,:] = self.mh_step(S[i,:], data)
@@ -21,20 +22,20 @@ class MetropolisHastings():
     def mh_step(self, theta, data):
         theta_new = self.get_theta_new(theta)
         log_alpha = self.get_log_alpha(theta, theta_new, data)
-        log_u = torch.log(torch.rand(1))/ data.size(0)
+        log_u = np.log(npr.rand(1))/ data.size(0)
         if log_u < log_alpha:
             theta = theta_new
         return theta
 
     def get_theta_new(self, theta):
-        return theta + self.get_stepsize()*torch.randn(2)
+        return theta + self.get_stepsize()*npr.randn(2)
 
     def get_log_alpha(self, theta, theta_new, data):
         lkhd = self.get_log_lkhd(theta_new, data) - self.get_log_lkhd(theta, data) # lkhd_new - lkhd_old
-        return torch.mean(lkhd)
+        return np.mean(lkhd)
 
     def get_log_lkhd(self, theta, data):
-        return -(((data - theta[0])/theta[1])**2)/2 - torch.log(theta[1]) # -((data-mu)/sig)**2 - log(sig)
+        return -(((data - theta[0])/theta[1])**2)/2 - np.log(theta[1]) # -((data-mu)/sig)**2 - log(sig)
 
     def get_stepsize(self):
-        return 0.5/torch.sqrt(torch.tensor([self.N])) # 0.5/sqrt(N)
+        return 0.5/np.sqrt(self.N) # 0.5/sqrt(N)
